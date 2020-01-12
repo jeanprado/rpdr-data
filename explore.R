fe20cc125b693dee2ed23f4e6179ef5e346eb4b3
Jean Prado <jeangrprado@gmail.com>
2020-01-12 16:13:37 -0300

    adds graph of lipsyncs by song author

--- explore.R
+++ explore.R
@@ -7,9 +7,10 @@ rpdr_rankings <- read_csv('data/rpdr_rankings.csv')
 rpdr_episodes <- read_csv('data/rpdr_episodes.csv')
 rpdr_lipsyncs <- read_csv('data/rpdr_lipsyncs.csv')
 
-# gets season's scores based on Dusted or Busted Scoring System -----------
-# [view metodology](https://rupaulsdragrace.fandom.com/wiki/%22Dusted_or_Busted%22_Scoring_System)
 
+# calculates contestant's scores ------------------------------------------
+
+## [gets season's scores based on Dusted or Busted Scoring System](https://rupaulsdragrace.fandom.com/wiki/%22Dusted_or_Busted%22_Scoring_System)
 base_scores <- rpdr_rankings %>% distinct(episode_placement) %>% 
   add_column(points=c(0, 2, -1, 1, -2, -2, rep(NA, 7), -2),
              performance=c(5, 10, 2.5, 7.5, 0, 0, rep(NA, 7), -2))
@@ -18,16 +19,15 @@ no_episodes <- rpdr_episodes %>% group_by(season_number) %>% summarize(episodes=
 
 ## calculates scores and weights on number of episodes
 rpdr_rankings <- rpdr_rankings %>% left_join(base_scores) %>% left_join(no_episodes) %>% 
-  mutate(points_episodes=points/episodes, performance_episodes=performance/episodes)
+  mutate(points_episodes=round((points/episodes)*10, digits=1),
+         performance_episodes=round((performance/episodes)*10, digits=1))
 
 rpdr_scores <- rpdr_rankings %>% group_by(season_number, contestant_name) %>%
   summarize(points=sum(points, na.rm=T),
             performance=sum(performance, na.rm=T),
             points_episodes=sum(points_episodes, na.rm=T),
-            performance_episodes=sum(performance_episodes, na.rm=T)) %>% arrange(desc(performance_episodes))
-
-rpdr_scores <- left_join(rpdr_scores, rpdr_contestants %>%
-                           select(season_number, contestant_name, season_outcome)) %>%
+            performance_episodes=sum(performance_episodes, na.rm=T)) %>% arrange(desc(performance_episodes)) %>% 
+            left_join(rpdr_contestants %>% select(season_number, contestant_name, season_outcome)) %>%
                            arrange(desc(performance_episodes), desc(points_episodes))
 
 ## comparison of balancing or not scores by number of season episodes
@@ -35,7 +35,7 @@ comparison_scores <- tibble(general=(rpdr_scores %>% arrange(desc(performance), 
                      weighted=rpdr_scores[1:10,"contestant_name"])
 
 ## graph of performance scores (weighted)
-ggplot(rpdr_scores[1:10,], aes(fct_reorder(contestant_name, performance), performance,
+ggplot(rpdr_scores[1:15,], aes(fct_reorder(contestant_name, performance_episodes), performance_episodes,
                                fill=as.factor(season_number))) + coord_flip() + theme_minimal() +
   geom_col() + labs(x="contestant") + guides(fill=F) + theme(panel.grid.major = element_blank(),
                                                              panel.grid.minor = element_blank())
